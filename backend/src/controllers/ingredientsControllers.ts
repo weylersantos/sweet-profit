6791c87cacf071d676d708be4af39f87805037fe
Weyler Santos <weyler.kleber@outlook.com>
2026-01-21 21:18:11 -0300

    feat: Add ingredient endpoints and new column in schema

---
+++ backend/src/controllers/ingredientsControllers.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { prisma } from "../config/db.js";
+
+type ingredient = {
+  userId: string;
+  name: string;
+  category: "KILOGRAMAS" | "GRAMAS" | "LITROS" | "ML" | "UNIDADE";
+  amount: number;
+  price: number;
+  description: string;
+};
+
+export const getAll = async (req: Request, res: Response) => {
+  const ingredients = await prisma.ingredient.findMany();
+
+  return res.status(200).json(ingredients);
+};
+
+export const getById = async (req: Request, res: Response) => {
+  const ingredientId = req.params.id as string;
+
+  if (!ingredientId) {
+    return res.status(400).json({ message: "Ingredient ID is required" });
+  }
+
+  try {
+    const result = await prisma.ingredient.findUnique({
+      where: { id: ingredientId },
+    });
+
+    if (!result) {
+      return res.status(404).json({ message: "Ingredient not found" });
+    }
+
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(500).json({ error: (err as Error).message });
+  }
+};
+
+export const create = async (req: Request, res: Response) => {
+  const { userId, data } = req.body;
+
+  const listItem = data.map((item: ingredient) => ({
+    createdBy: userId,
+    ...item,
+  }));
+
+  try {
+    const result = await prisma.ingredient.createMany({
+      data: listItem,
+    });
+
+    return res.status(201).json({
+      status: "success",
+      message: "Ingredient was created",
+    });
+  } catch (err) {
+    return res.status(500).json({ error: (err as Error).message });
+  }
+};
+
+export const removeById = async (req: Request, res: Response) => {
+  const ingredientId = req.params.id as string;
+
+  if (!ingredientId) {
+    return res.status(400).json({ message: "Ingredient ID is required" });
+  }
+
+  try {
+    const result = await prisma.ingredient.delete({ where: { id: ingredientId } });
+
+    if (!result) {
+      return res.status(404).json({ message: "Ingredient not found" });
+    }
+
+    return res.status(200).json({ message: "Ingredient deleted" });
+  } catch (err) {
+    return res.status(500).json({ error: (err as Error).message });
+  }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import recipesRoutes from "./routes/recipesRoutes.js";
+import ingredientRoutes from "./routes/ingredientsRoutes.js";
 
 connectDB();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/recipes", recipesRoutes);
+app.use("/ingredients", ingredientRoutes);
 
 app.listen(3000, () => {
   console.log("APP running on PORT 3000");
